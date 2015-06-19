2a9cae16015f236bc06c3d1836d5884db0a66115
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-06-19 18:54:59 -0400

    color, listview, keylines

---
+++ app/src/main/java/com/fisheradelakin/mycontacts/Contact.java
@@ -0,0 +1,17 @@
+package com.fisheradelakin.mycontacts;
+
+/**
+ * Created by Fisher on 6/19/15.
+ */
+public class Contact {
+
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
